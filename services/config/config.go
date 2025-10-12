cf422c309336bdb5ca1be18d9d1d26a8bd2d2a02
Alberto Boccolini <48589525+albertoboccolini@users.noreply.github.com>
2025-10-12 12:43:45 +0200

    Improved services separation (#14)
    * Improved services separation
    
    * Update services/config/config.go

---
+++ services/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func GetConfigDir() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configDir := filepath.Join(home, ".config", "ob")
+	return configDir
+}
+
+func CreateConfigDir() {
+	if err := os.MkdirAll(GetConfigDir(), 0755); err != nil {
+		log.Fatal("Error creating config directory:", err)
+	}
+}
+
+var (
+	configDir  = GetConfigDir()
+	PidFile    = filepath.Join(configDir, "ob.pid")
+	LogFile    = filepath.Join(configDir, "ob.log")
+	ConfigFile = filepath.Join(configDir, "vault.path")
+)
