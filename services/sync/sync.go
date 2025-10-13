4b3ad84d151a466f71c271e15efd1682628b2352
albertoboccolini <albertoboccolini03@gmail.com>
2025-10-13 16:21:46 +0200

    Improved sync logic

--- services/sync/sync.go
+++ services/sync/sync.go
@@ -4,39 +4,40 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"ob/services/config"
 	"ob/services/git"
 )
 
-func syncToRemote(vaultPath string) error {
+func syncToRemote(vaultPath string) {
 	err := git.PullIfNeeded(vaultPath)
 	if err != nil {
-		return err
+		log.Println("Error syncing vault:", err)
+		return
 	}
 
 	log.Println("Sync to remote successful.")
-	return nil
 }
 
-func syncVault(vaultPath string) error {
+func syncVault(vaultPath string) {
 	hasChanges, err := git.HasUncommittedChanges(vaultPath)
 	if err != nil {
-		return err
+		log.Println("Error checking for uncommitted changes:", err)
+		return
 	}
 
 	if hasChanges {
 		err = git.CommitChanges(vaultPath)
 		if err != nil {
-			return err
+			log.Println("Error committing changes:", err)
+			return
 		}
 
 		log.Println("Changes committed successfully.")
-		return nil
+		return
 	}
-
-	return nil
 }
 
 func RunDaemon() {
@@ -45,7 +46,7 @@ func RunDaemon() {
 		log.Fatal("Error reading vault path from config:", err)
 	}
 
-	vaultPath := string(data)
+	vaultPath := strings.TrimSpace(string(data))
 	config.CreateConfigDir()
 
 	f, err := os.OpenFile(config.GetLogFile(), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -72,22 +73,17 @@ func RunDaemon() {
 	defer syncVaultTicker.Stop()
 
 	log.Println("Starting sync operations...")
+
+	syncVault(vaultPath)
+	syncToRemote(vaultPath)
+
 	for {
 		select {
-		case <-syncToRemoteTicker.C:
-			go func() {
-				err := syncToRemote(vaultPath)
-				if err != nil {
-					log.Println("Error syncing to remote:", err)
-				}
-			}()
 		case <-syncVaultTicker.C:
-			go func() {
-				err := syncVault(vaultPath)
-				if err != nil {
-					log.Println("Error syncing vault:", err)
-				}
-			}()
+			syncVault(vaultPath)
+		case <-syncToRemoteTicker.C:
+			syncVault(vaultPath)
+			syncToRemote(vaultPath)
 		}
 	}
 }
